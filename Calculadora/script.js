0bfda0a3a4a368ac88a125b1c568341e44fcfe86
GustavoSilva3164 <gustavosilva2008br@gmail.com>
2024-09-02 20:31:32 -0300

    Create script.js

---
+++ Calculadora/script.js
@@ -0,0 +1,15 @@
+const num1 = 8; 
+const num2 = 4; 
+
+// Realizando operações
+let soma = num1 + num2;
+let sub = num1 - num2;
+let mult = num1 * num2;
+let div = num1 / num2;
+
+console.log(`Exemplo de números ${num1} e ${num2}`);
+console.log(`Resultado`);
+console.log(`${num1} + ${num2} = ${soma}`);
+console.log(`${num1} - ${num2} = ${sub}`);
+console.log(`${num1} * ${num2} = ${mult}`);
+console.log(`${num1} / ${num2} = ${div}`);
