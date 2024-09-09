008268245aea52384b88db8e854a753ff23f0fe7
GuatavoSilva3164 <gustavosilva2008br@gmail.com>
2024-09-09 18:36:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const nome = 'John';
+const sobrenome = 'Wick';
+const curso = 'Desenvolvimento de Sistemas';
+const anoNascimento = 1989; 
 
+console.log('Olá ' + nome + ' ' + sobrenome + ', então você estuda ' + curso + ' e tem ' + (2024 - anoNascimento) + ' anos. Legal!');
