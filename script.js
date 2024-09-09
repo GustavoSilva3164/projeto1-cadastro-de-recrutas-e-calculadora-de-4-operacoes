0f115ac8a687ebf0daf5731169943f324f02c69b
GuatavoSilva3164 <gustavosilva2008br@gmail.com>
2024-09-09 18:40:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const curso = 'Desenvolvimento de Sistemas';
 const anoNascimento = 1989; 
 
 console.log('Olá ' + nome + ' ' + sobrenome + ', então você estuda ' + curso + ' e tem ' + (2024 - anoNascimento) + ' anos. Legal!');
+
+const num1 = 8; 
+const num2 = 4; 
+
+// Realizando operações
+let soma = num1 + num2;
+let sub = num1 - num2;
+let mult = num1 * num2;
+let div = num1 / num2;
+
+console.log(`Exemplo de números ${num1} e ${num2}`);
+console.log(`Resultado`);
+console.log(`${num1} + ${num2} = ${soma}`);
+console.log(`${num1} - ${num2} = ${sub}`);
+console.log(`${num1} * ${num2} = ${mult}`);
+console.log(`${num1} / ${num2} = ${div}`);
